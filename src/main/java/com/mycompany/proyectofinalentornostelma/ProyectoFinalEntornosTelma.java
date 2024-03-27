0c9685601b2115d8bbe8c520886b7e9500c81c25
Telma <teteixeirat01@larioja.edu.es>
2024-03-27 11:02:00 +0100

    Codigo inicial

---
+++ src/main/java/com/mycompany/proyectofinalentornostelma/ProyectoFinalEntornosTelma.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinalentornostelma;
+
+/**
+ *
+ * @author Diurno
+ */
+public class ProyectoFinalEntornosTelma {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
